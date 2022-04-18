384a7d1f36853a34192168b3b736242b2f7e7a50
Alale <Doebi.Alale@ehi.com>
2022-04-18 11:55:22 -0500

    First Feature

--- MouseJigglerPowershell.ps1
+++ MouseJigglerPowershell.ps1
@@ -1,18 +1,19 @@
 
 # MouseJiggler Powershell Script
 # Written by Doebi Alale
+# Cashapp $doebialale
 # https://github.com/doebialale/Mouse-Jiggler
 
 
 Clear-Host
 Echo "Keep-alive with Scroll Lock..."
 
-$WShell = New-Object -com "Wscript.Shell"
+# $WShell = New-Object -com "Wscript.Shell"
 
 while ($true)
 {
   $WShell.sendkeys("{SCROLLLOCK}")
-  Start-Sleep -Seconds 60
+  Start-Sleep -Seconds 2
   $WShell.sendkeys("{SCROLLLOCK}")
-  Start-Sleep -Seconds 60
+  Start-Sleep -Seconds 2
 }
